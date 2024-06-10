d9a5007dc1a39ee3f80907ee1024e47abfabd923
Izuchukwu Mba <izuchukwu@meta.com>
2024-06-10 14:16:42 -0700

    UI Component for the Navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import "../styles/Navbar.css";
+import logo from "../assets/new-flixster-logo.png";
+
+function Navbar() {
+  return (
+    <div id="navbar-container">
+      <div id="navbar">
+        <div id="logo-container">
+          {" "}
+          <img id="logo" src={logo} />
+        </div>
+        <div id="nav-texts">
+          <div id="app-name">Flixster Flick</div>
+          <div id="nav-sections">
+            <div>Home</div>
+            <div>Categories</div>
+            <div>Watchlist</div>
+            <div>Settings</div>
+          </div>
+        </div>
+        <div id="user-welcome">Hello, Izuchukwu</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
